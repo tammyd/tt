b9cf4eb04cf68dd1ced22944b31a2bbc98f2a4d8
Tammy D <tammyd@gmail.com>
2013-03-29 22:00:10 -0700

    setuptools

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup (
+    name = "tt - Time Tracker",
+    version = "0.1",
+    description="A simple command line utility to track the time you work on things.",
+    author="Tammy D",
+    author_email="tammyd@gmail.com", 
+    url="https://github.com/tammyd",
+    package_data = {'': ['*.md']},
+    packages = find_packages(exclude="test"),
+    entry_points = {
+        'console_scripts': ['tt=tt:main']
+    },
+
+    download_url = "https://github.com/tammyd/tt",
+    zip_safe = True,
+    install_requires=['prettytable>=0.7.1','parsedatetime>=1.1.2']
+)
